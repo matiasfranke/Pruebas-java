88018253386c742c2484dbbc8b3e083bda7dd781
matiasfranke <mfranke@navent.com>
2022-03-17 16:31:57 -0300

    ejemplos de enum

---
+++ src/OOP/clases/Enum/EnumEstados.java
@@ -0,0 +1,2 @@
+package OOP.clases.Enum;public enum EnumEstados {
+}
---
+++ src/OOP/clases/Enum/TestEnum.java
@@ -0,0 +1,12 @@
+package OOP.clases.Enum;
+
+import OOP.clases.definicion.Persona;
+
+public class TestEnum {
+    public static void main(String[] args) {
+        Persona pepe = new Persona("pepe");
+        Persona juan = new Persona("juan");
+        pepe.come();
+        juan.trabaja();
+    }
+}
